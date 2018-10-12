bdb42487d761dd6fbc4efc2b334d8cd58b7fcaa9
Das MA <das@MA.frontierlocal.net>
2018-10-12 08:01:23 -0700

    Starting out

--- data_structures/ex_1/binary_search_tree.py
+++ data_structures/ex_1/binary_search_tree.py
@@ -5,9 +5,11 @@ class BinarySearchTree:
     self.right = None
 
   def depth_first_for_each(self, cb):
+
     pass    
 
   def breadth_first_for_each(self, cb):
+    
     pass
 
   def insert(self, value):
